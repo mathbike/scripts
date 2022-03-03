c507bab07cee183034f87180f850fa76e9572503
mathbike <mathbike8@gmail.com>
2022-03-03 02:39:04 +0000

    update

--- startup.sh
+++ startup.sh
@@ -4,18 +4,14 @@
 xrandr --output eDP-1 --brightness 0.6
 
 # dwm status bar
-date=$(/bin/date +"%a %b %d")
-time=$(/bin/date +"%H:%M")
-batt() {
-	cat "/sys/class/power_supply/BAT0/capacity"
-}
-bat=$(batt)
+#date=$(/bin/date +"%a %b %d")
+#time=$(/bin/date +"%H:%M")
+#batt() {
+#	cat "/sys/class/power_supply/BAT0/capacity"
+#}
+#bat=$(batt)
 
 # set dwm status bar
-xsetroot -name "[$date] [$time] [$bat]"
+#xsetroot -name "[$date] [$time] [$bat]"
 
-#while true
-#do
-#	xsetroot -name "[$date] [$time] [$bat]";
-#	sleep 1s;
-#done
+
