9f381656433f7132ff5d5e86a4f8778dd74bc9b1
mathbike <mathbike8@gmail.com>
2022-03-08 07:24:52 +0000

    update

--- dwmstartup.sh
+++ dwmstartup.sh
@@ -2,9 +2,10 @@
 
 # dwmstartup
 
-# brave
+# W: brave
 tag1() {
 	xdotool key alt+1 sleep 0.1
+	# 1
 	xdotool key Shift+alt+Return sleep 0.1
 	xdotool type "brave" &&  sleep 0.1
 	xdotool key Return sleep 1
@@ -12,26 +13,34 @@ tag1() {
 	xdotool key Shift+alt+c sleep 0.1
 }
 
-# lf / bashtop
+# F: lf / bashtop
 tag2() {
 	xdotool key alt+2 sleep 0.1
+	# 1
 	xdotool key Shift+alt+Return sleep 0.1
+	xdotool type "sudo vim TODO.txt" && sleep 0.1
+	xdotool key Return sleep 0.1
+	# 2
 	xdotool key Shift+alt+Return sleep 0.1
 	xdotool type "lf" && sleep 0.1
 	xdotool key Return sleep 0.1
+	# 3
 	xdotool key Shift+alt+Return sleep 0.1
 	xdotool type "bashtop" && sleep 0.1
 	xdotool key Return sleep 0.1
 }
 
-# archmathbike.sh
+# A: archmathbike.sh
 tag3() {
 	xdotool key alt+3 sleep 0.1
+	# 1
 	xdotool key Shift+alt+Return sleep 0.1
+	# 2
 	xdotool key Shift+alt+Return sleep 0.1
 	xdotool type "cd archmathbike" && sleep 0.1
 	xdotool key Return sleep 0.1
 	xdotool key ctrl+l sleep 0.1
+	# 3
 	xdotool key Shift+alt+Return sleep 0.1
 	xdotool type "cd archmathbike" && sleep 0.1
 	xdotool key Return sleep 0.1
@@ -41,20 +50,26 @@ tag3() {
 	xdotool key Return sleep 0.1
 }
 
-# commands / TODO
+# C: commands / TODO
 tag4() {
 	xdotool key alt+4 sleep 0.1
-	xdotool key Shift+alt+Return sleep 0.1
-	xdotool type "cd commands" && sleep 0.1
-	xdotool key Return sleep 0.1
-	xdotool type "sudo vim TODO.txt" && sleep 0.1
-	xdotool key Return sleep 0.1
-	xdotool type "5790" && sleep 0.1
-	xdotool key Return sleep 0.1
+	# 1
 	xdotool key Shift+alt+Return sleep 0.1
 	xdotool type "cd commands" && sleep 0.1
 	xdotool key Return sleep 0.1
 	xdotool key ctrl+l sleep 0.1
+	xdotool type "ls -l" && sleep 0.1
+	xdotool key Return sleep 0.1
+	xdotool type "sudo vim" && sleep 0.
+	# 2
+	xdotool key Shift+alt+Return sleep 0.1
+	xdotool type "cd commands" && sleep 0.1
+	xdotool key Return sleep 0.1
+	xdotool key ctrl+l sleep 0.1
+	xdotool type "ls -l" && sleep 0.1
+	xdotool key Return sleep 0.1
+	xdotool type "sudo vim" && sleep 0.1
+	# 3
 	xdotool key Shift+alt+Return sleep 0.1
 	xdotool type "cd commands" && sleep 0.1
 	xdotool key Return sleep 0.1
