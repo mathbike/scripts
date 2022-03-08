0cb6add36ada2226cf55ce90f6ec74a9d82c422c
mathbike <mathbike8@gmail.com>
2022-03-08 05:30:05 +0000

    update

---
+++ dwmstartup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# dwmstartup
+
+# tag 1, brave
+xdotool key alt+3 sleep 0.1
+xdotool key Shift+alt+Return sleep 0.1
+xdotool type brave && sleep 0.1
+xdotool key Return sleep 1
+xdotool key alt+k sleep 0.1
+xdotool key Shift+alt+c
+
--- startup.sh
+++ startup.sh
@@ -3,15 +3,3 @@
 # set screen brightness
 xrandr --output eDP-1 --brightness 0.6
 
-# dwm status bar
-#date=$(/bin/date +"%a %b %d")
-#time=$(/bin/date +"%H:%M")
-#batt() {
-#	cat "/sys/class/power_supply/BAT0/capacity"
-#}
-#bat=$(batt)
-
-# set dwm status bar
-#xsetroot -name "[$date] [$time] [$bat]"
-
-
