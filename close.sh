af50b1d0c05986913c01442d8a7b9c0d640bf427
mathbike <mathbike8@gmail.com>
2022-03-08 12:22:10 +0000

    update

--- close.sh
+++ close.sh
@@ -21,6 +21,26 @@ close() {
 	xdotool key Shift+alt+c sleep 0.1
 	xdotool key Shift+alt+c sleep 0.1
 	xdotool key Shift+alt+c sleep 0.1
+	# tag 5
+	xdotool key alt+5 sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	# tag 6
+	xdotool key alt+6 sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	# tag 7
+	xdotool key alt+7 sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	# tag 8
+	xdotool key alt+8 sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
+	xdotool key Shift+alt+c sleep 0.1
 }
 
 close
