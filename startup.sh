ca87fddef2daafee409509d023a466e514ede863
mathbike <mathbike8@gmail.com>
2022-05-27 22:52:33 -0400

    update startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
 # set screen brightness
-xrandr --output eDP-1 --brightness 1
+#xrandr --output eDP-1 --brightness 1
+
+asus() {
+	# disable touchscreen
+	xinput -disable 13
+	# disable webcam
+	xinput -disable 12
+}
+
+hp() {
+	# disable laptop display
+	xrandr --output eDP-1 --off
+	# disable touchscreen
+	xinput -disable 15
+	# disable webcam
+	xinput -disable 13
+       # disable IR camera
+	xinput -disable 14       
+}
+
+T420() {
+	:
+}
+
+X220() {
+	:
+}
+
+#asus
+hp
+#T420
+#X220
 
